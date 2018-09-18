3408c868d60f3fd5de2aa50c45176f08160cbe55
Edwin van de Ven <edwin@simplycode.nl>
2018-09-18 20:19:19 +0200

    Add logisticsitems.py & update __init__.py

--- exactonline/api/__init__.py
+++ exactonline/api/__init__.py
@@ -17,8 +17,11 @@ from .contacts import Contacts
 from .invoices import Invoices
 from .ledgeraccounts import LedgerAccounts
 from .receivables import Receivables
+from .logisticsitems import LogisticsItems
 from .relations import Relations
+from .bankaccounts import BankAccounts
 from .vatcodes import VatCodes
+
 
 
 class ExactApi(
@@ -37,5 +40,7 @@ class ExactApi(
     invoices = Invoices.as_property()
     ledgeraccounts = LedgerAccounts.as_property()
     receivables = Receivables.as_property()
+    logisticsitems = LogisticsItems.as_property()
     relations = Relations.as_property()
+    bankaccounts = BankAccounts.as_property()
     vatcodes = VatCodes.as_property()
